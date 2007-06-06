7efcbb4eb0417da17b21cb50c156b971e26e6560
soeren <soeren@18689620-072c-0410-bf1f-983e1a6c8eca>
2007-06-06 11:03:51 +0000

    Created branch joomlaxplorer_1.x.

---
+++ branches/joomlaxplorer_1.x/install.joomlaxplorer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* @version $Id$
+* @package joomlaXplorer
+* @copyright (C) 2005-2006 Soeren
+* @license GNU / GPL
+* @author soeren
+* joomlaXplorer is Free Software
+*/
+function com_install(){
+	global $database;
+
+	$database->setQuery( "SELECT id FROM #__components WHERE admin_menu_link = 'option=com_joomlaxplorer'" );
+	$id = $database->loadResult();
+
+	//add new admin menu images
+	$database->setQuery( "UPDATE #__components SET admin_menu_img = '../administrator/components/com_joomlaxplorer/images/joomla_x_icon.png', admin_menu_link = 'option=com_joomlaxplorer' WHERE id=$id");
+	$database->query();
+}
+?>
