32a636cae1c5b1c39b501236177fdc7e9b291539
soeren <soeren@18689620-072c-0410-bf1f-983e1a6c8eca>
2007-06-22 10:57:16 +0000

    minor changes

--- build_component.sh
+++ build_component.sh
@@ -11,7 +11,7 @@
 # It's usually globally accessible (in the directory /usr/local/bin/)
 
 DATE=$(date +%Y%m%d)
-PATH='/home/soeren/Joomla/components/com_joomlaxplorer'
+PATH='/home/soeren/Joomla/components/joomlaxplorer_1.x'
 cd $PATH
 /usr/local/lib/p7zip/7za a -ttar -r $PATH/com_joomlaxplorer.tar
 /usr/local/lib/p7zip/7za d -r $PATH/com_joomlaxplorer.tar .svn/
