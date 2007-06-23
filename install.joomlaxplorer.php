abd28c0c0389fae25301bb4759f917d23583f231
soeren <soeren@18689620-072c-0410-bf1f-983e1a6c8eca>
2007-06-23 05:56:32 +0000

    changed the installer to extract a ZIP with all scripts (like Codepress and ExtJS) to save memory. Working with ZIP as distribution format now.

--- install.joomlaxplorer.php
+++ install.joomlaxplorer.php
@@ -8,8 +8,21 @@
 * joomlaXplorer is Free Software
 */
 function com_install(){
-	global $database;
-
+	global $database, $mosConfig_absolute_path;
+	require_once($mosConfig_absolute_path . "/administrator/components/com_joomlaxplorer/include/functions.php");
+	require_once($mosConfig_absolute_path . "/administrator/components/com_joomlaxplorer/libraries/Archive.php");
+	
+	jx_RaiseMemoryLimit( '16M' );
+	error_reporting( E_ALL ^ E_NOTICE );
+	
+	$archive_name = $mosConfig_absolute_path . "/administrator/components/com_joomlaxplorer/scripts.zip";
+	$archive_as_dir = $archive_name.'/';
+	$extract_dir = $mosConfig_absolute_path . "/administrator/components/com_joomlaxplorer/";
+	
+	$result = File_Archive::extract( $archive_as_dir, $extract_dir );
+	if( !PEAR::isError( $result )) {
+		unlink( $archive_name );
+	}
 	$database->setQuery( "SELECT id FROM #__components WHERE admin_menu_link = 'option=com_joomlaxplorer'" );
 	$id = $database->loadResult();
 
