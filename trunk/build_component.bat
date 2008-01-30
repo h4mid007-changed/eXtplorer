da16b75c74f47dffb90841f2d44f1a8598869b43
soeren <soeren@18689620-072c-0410-bf1f-983e1a6c8eca>
2008-01-30 08:21:34 +0000

    build_script fix; Installation Fix; CodePress Area Auto-Width Fix

--- trunk/build_component.bat
+++ trunk/build_component.bat
@@ -1,14 +1,13 @@
-#!\bin\sh
-# ----------------------------------------------------------------------------
-#
-# Component Install Archive Builder
-#
-# This file is part of eXtplorer
-#
-# ----------------------------------------------------------------------------
+rem ----------------------------------------------------------------------------
+rem
+rem Component Install Archive Builder
+rem
+rem This file is part of eXtplorer
+rem
+rem ----------------------------------------------------------------------------
 
-# YOU MUST HAVE INSTALLED THE 4.x BETA VERSION OF p7zip (the command line version of 7-Zip for Unix\Linux).
-# It's usually globally accessible (in the directory \usr\local\bin\)
+rem YOU MUST HAVE INSTALLED THE 4.x VERSION OF 7zip
+rem Please update the program path here accordingly
 
 
 set PATH=C:\DOWNLOADS\Joomla\components\extplorer
@@ -20,7 +19,7 @@ C:\Programme\7-Zip\7z.exe d -r %PATH%\scripts.zip .svn\
 
 C:\Programme\7-Zip\7z.exe a -tzip -r %PATH%\com_extplorer.zip
 C:\Programme\7-Zip\7z.exe d -r %PATH%\com_extplorer.zip .svn\
-C:\Programme\7-Zip\7z.exe d %PATH%\com_extplorer.zip scripts\
+C:\Programme\7-Zip\7z.exe d %PATH%\com_extplorer.zip scripts\ archive\
 
 C:\Programme\7-Zip\7z.exe d -r %PATH%\com_extplorer.zip build_component.sh build_component.bat .project .projectOptions .cache preinstall.php README_PREINSTALL.txt
 
