2047dfd47d28557a9faffc7b151b0c1db4e123a0
ryu_ms <ryu_ms@18689620-072c-0410-bf1f-983e1a6c8eca>
2009-01-26 14:35:19 +0000

    split conf file. because multi-byte support needs PHP extension "mbstring". only when server have mbstring, require_once conf/mb_conf.php.

---
+++ trunk/config/mb_conf.php
@@ -0,0 +1,12 @@
+<?php
+
+// Support Multi-byte.
+// NEED PHP extension "mbstring".
+
+// System Charset
+$GLOBALS["system_charset"] = 'UTF-8';
+
+// Set Locale
+setlocale(LC_ALL, 'en_US.UTF8');
+
+?>
