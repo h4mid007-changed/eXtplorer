176721c4b4ad038bdaa9bb5c784e219ce60fabb0
soeren <soeren@18689620-072c-0410-bf1f-983e1a6c8eca>
2010-11-06 08:45:47 +0000

    switched to ext-base adapter for ExtJS

--- trunk/include/login.php
+++ trunk/include/login.php
@@ -107,9 +107,8 @@ function login() {
 	$GLOBALS['mainframe']->setPageTitle( ext_Lang::msg('actlogin') );
 	$GLOBALS['mainframe']->addcustomheadtag( '
 		<script type="text/javascript" src="'. _EXT_URL . '/fetchscript.php?'
-			.'&amp;subdir[0]=scripts/yui/utilities/&amp;file[0]=utilities.js'
-			.'&amp;subdir[1]=scripts/extjs3/adapter/yui/&amp;file[1]=ext-yui-adapter.js'
-			.'&amp;subdir[2]=scripts/extjs3/&amp;file[2]=ext-all.js&amp;gzip=1"></script>
+			.'subdir[0]=scripts/extjs3/adapter/ext/&amp;file[0]=ext-base.js'
+			.'&amp;subdir[1]=scripts/extjs3/&amp;file[1]=ext-all.js&amp;gzip=1"></script>
 		<script type="text/javascript" src="'. $GLOBALS['script_name'].'?option=com_extplorer&amp;action=include_javascript&amp;file=functions.js"></script>
 		<link rel="stylesheet" href="'. _EXT_URL . '/fetchscript.php?subdir[0]=scripts/extjs3/resources/css/&file[0]=ext-all.css&amp;subdir[1]=scripts/extjs3/resources/css/&file[1]=xtheme-blue.css&amp;gzip=1" />');
 
